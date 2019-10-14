4675d8f9369ef2a22d00116d49bbbf78f1f73d26
Ubuntu <ubuntu@ip-172-31-82-212.ec2.internal>
2019-10-14 12:40:12 +0000

    added host port and debug into app.py file

--- app.py
+++ app.py
@@ -9,7 +9,4 @@ def index():
     return 'Hello MEMpy'
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-    host = os.getenv('IP', '0.0.0.0')
-    app.run(port=port, host=host)
-    
+    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True) 
